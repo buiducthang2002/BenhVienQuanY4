2aa536fcbc345ffbab6c4fb8160926fa32617073
Bùi Đức Thắng <buiducthang2002@gmail.com>
2025-10-15 16:02:26 +0700

    feat(models): add BenhAn entity

---
+++ APP/Models/BenhAn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APP.Models
+{
+    [Table("tbl_benhan_benhantheobn")]
+    public class BenhAn
+	{
+        [Key]
+        public string makcb { get; set; }
+        public string maubenhan { get; set; }
+        public string sobenhan { get; set; }
+        public DateTime ngaylam { get; set; }
+        public int manv { get; set; }
+        public string? daky { get; set; }
+    }
+}
+
